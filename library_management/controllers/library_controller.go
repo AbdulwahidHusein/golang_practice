a3a16222be546b241821461d1c697960172b7287
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-07-29 14:33:05 -0700

    library contruller crud on book completed

--- library_management/controllers/library_controller.go
+++ library_management/controllers/library_controller.go
@@ -1,1 +1,117 @@
 package controllers
+
+import (
+	"encoding/json"
+	"library_management/models"
+	"library_management/services"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type LibraryController struct {
+	LibraryService *services.LibraryService
+}
+
+func NewLibraryController(service *services.LibraryService) *LibraryController {
+	return &LibraryController{service}
+}
+
+func (l *LibraryController) GetBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := l.LibraryService.GetBooks()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func (l *LibraryController) GetBook(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+	bookId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	book, err := l.LibraryService.GetBook(bookId)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(book)
+}
+
+func (l *LibraryController) GetAvailableBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := l.LibraryService.GetAvailableBooks()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func (l *LibraryController) AddBook(w http.ResponseWriter, r *http.Request) {
+
+	var book models.Book
+	err1 := json.NewDecoder(r.Body).Decode(&book)
+	if err1 != nil {
+		http.Error(w, err1.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err2 := l.LibraryService.AddBook(&book)
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (l *LibraryController) UpdateBook(w http.ResponseWriter, r *http.Request) {
+
+	var book models.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (l *LibraryController) DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+	bookId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err2 := l.LibraryService.DeleteBook(bookId)
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func (l *LibraryController) BorrowBook(w http.ResponseWriter, r *http.Request) {
+
+	var borrow models.BorrowedBook
+	err := json.NewDecoder(r.Body).Decode(&borrow)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func (l *LibraryController) ReturnBook(w http.ResponseWriter, r *http.Request) {
+
+	var borrow models.BorrowedBook
+	err := json.NewDecoder(r.Body).Decode(&borrow)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
--- library_management/go.mod
+++ library_management/go.mod
@@ -3,6 +3,8 @@ module library_management
 go 1.22.5
 
 require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
