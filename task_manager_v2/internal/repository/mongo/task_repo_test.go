b2be17d17075435bfb4206b639d147f17f9e1d43
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-08 07:40:27 -0700

    task repository unit tested

---
+++ task_manager_v2/internal/repository/mongo/task_repo_test.go
@@ -0,0 +1,80 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"task_managemet_api/cmd/task_manager/config"
+	"task_managemet_api/cmd/task_manager/internal/domain"
+	"task_managemet_api/cmd/task_manager/internal/repository"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+type MongoTaskRepositoryTestSuite struct {
+	suite.Suite
+	client         *mongo.Client
+	repo           repository.TaskRepository // Use the interface type here
+	taskCollection *mongo.Collection
+}
+
+// SetupSuite runs once before all tests
+func (suite *MongoTaskRepositoryTestSuite) SetupSuite() {
+	cwd, _ := os.Getwd()
+	fmt.Println("test repo file runs in a ", cwd)
+	clientOptions := options.Client().ApplyURI(config.GetMongoURI())
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	require.NoError(suite.T(), err)
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+	require.NoError(suite.T(), err)
+
+	suite.client = client
+
+	suite.taskCollection = suite.client.Database("task_manager_db").Collection("tasks")
+	suite.repo = NewMongoTaskRepository(suite.client) // Instantiate correctly
+}
+
+// TearDownSuite runs once after all tests
+func (suite *MongoTaskRepositoryTestSuite) TearDownSuite() {
+	err := suite.client.Disconnect(context.TODO())
+	require.NoError(suite.T(), err)
+}
+
+// SetupTest runs before each test to ensure the collection is clean
+func (suite *MongoTaskRepositoryTestSuite) SetupTest() {
+	err := suite.taskCollection.Drop(context.TODO())
+	require.NoError(suite.T(), err)
+}
+
+// TestAddTask tests the AddTask method
+func (suite *MongoTaskRepositoryTestSuite) TestAddTask() {
+	task := &domain.Task{
+		Title:       "Test Task",
+		Description: "Test Description",
+		DueDate:     time.Now(),
+		Status:      "Pending",
+	}
+
+	err := suite.repo.AddTask(task)
+	require.NoError(suite.T(), err)
+
+	var result domain.Task
+	err = suite.taskCollection.FindOne(context.TODO(), bson.M{"title": "Test Task"}).Decode(&result)
+	require.NoError(suite.T(), err)
+	require.Equal(suite.T(), task.Title, result.Title)
+}
+
+// Add other tests like TestGetAllTasks, TestGetTaskById, etc...
+
+// Run the suite
+func TestMongoTaskRepositoryTestSuite(t *testing.T) {
+	suite.Run(t, new(MongoTaskRepositoryTestSuite))
+}
--- task_manager_v2/pkg/db/mongo.go
+++ task_manager_v2/pkg/db/mongo.go
@@ -2,22 +2,16 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
+	"task_managemet_api/cmd/task_manager/config"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func ConnectToMongo() (*mongo.Client, error) {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
-	MongoURI := os.Getenv("MONGODB_URI")
-	fmt.Print("mongo uri: ", MongoURI)
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoURI))
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(config.GetMongoURI()))
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
