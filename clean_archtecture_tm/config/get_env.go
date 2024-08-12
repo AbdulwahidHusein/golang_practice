50788ca8993ae5785d37eaed4eb2e44189830817
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-12 04:52:25 -0700

    repository tests commented out

--- clean_archtecture_tm/config/get_env.go
+++ clean_archtecture_tm/config/get_env.go
@@ -11,12 +11,13 @@ import (
 var Envs map[string]string
 
 func GetEnvs() map[string]string {
+
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+		log.Println("No .env file found or failed to load it")
 	}
 
 	if Envs == nil {
-		Envs = make(map[string]string) // Initialize the map
+		Envs = make(map[string]string)
 	}
 
 	for _, env := range os.Environ() {
@@ -27,39 +28,3 @@ func GetEnvs() map[string]string {
 	}
 	return Envs
 }
-
-// func GetSecretKey() string {
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("No .env file found")
-// 	}
-// 	return os.Getenv("SECRET_KEY")
-// }
-
-// func GetMongoURI() string {
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("No .env file found")
-// 	}
-// 	MongoURI := os.Getenv("MONGODB_URI")
-// 	return MongoURI
-// }
-
-// func GetTokenExpiry() (int, int) {
-
-// 	if err := godotenv.Load(); err != nil {
-// 		log.Println("No .env file found")
-// 	}
-// 	AcessExpiry := os.Getenv("ACCESSES_TOKEN_EXPIRY")
-// 	RefreshExpiry := os.Getenv("REFRESH_TOKEN_EXPIRY")
-
-// 	accessesExpiryInt, err := strconv.Atoi(AcessExpiry)
-// 	if err != nil {
-// 		accessesExpiryInt = 24 // default to 24 hours if TOKEN_EXPIRY is not set or invalid
-// 	}
-// 	refteshExpiryInt, err := strconv.Atoi(RefreshExpiry)
-
-// 	if err != nil {
-// 		refteshExpiryInt = 48
-// 	}
-
-// 	return (accessesExpiryInt), (refteshExpiryInt)
-// }
